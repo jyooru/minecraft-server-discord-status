017aafbbc9ce6eaa2f48ffbf093f8a8f0a6604a7
Joel <joel@joel.tokyo>
2022-04-21 21:39:05 +1000

    feat: configuration using arguments and environment variables

--- minecraft_server_discord_status/__main__.py
+++ minecraft_server_discord_status/__main__.py
@@ -1,4 +1,7 @@
 import asyncio
+from argparse import ArgumentParser, Namespace
+from os import environ
+from typing import Any, List, Optional
 
 import aiohttp
 from discord import AsyncWebhookAdapter, Webhook
@@ -7,31 +10,69 @@ from mcstatus import JavaServer
 from . import MinecraftServerDiscordStatus
 
 
+def environ_or_required(key: str) -> Any:
+    # https://stackoverflow.com/a/45392259
+    return {"default": environ.get(key)} if environ.get(key) else {"required": True}
+
+
+def parse_args(args: Optional[List[str]] = None) -> Namespace:
+    parser = ArgumentParser(
+        description="Updates a Discord message with the status of a Minecraft server"
+    )
+    parser.add_argument(
+        "--message-id",
+        help="ID of the Discord message to edit",
+        type=int,
+        **environ_or_required("MESSAGE_ID"),
+    )
+    parser.add_argument(
+        "--server-host",
+        help="Host of the Minecraft server to check",
+        **environ_or_required("SERVER_HOST"),
+    )
+    parser.add_argument(
+        "--server-port",
+        default=environ.get("SERVER_PORT"),
+        help="Port of the Minecraft server to check",
+        type=int,
+    )
+    parser.add_argument(
+        "--thumbnail",
+        default=environ.get("THUMBNAIL"),
+        help="URL of the thumbnail to use in the Discord embed",
+    )
+    parser.add_argument(
+        "--title",
+        default=environ.get("TITLE"),
+        help="Title of the Discord embed",
+    )
+    parser.add_argument(
+        "--webhook",
+        help="URL of the Discord webhook",
+        **environ_or_required("WEBHOOK"),
+    )
+
+    if args is None:
+        return parser.parse_args()
+    return parser.parse_args(args)
+
+
 async def async_main() -> None:
+    args = parse_args()
     async with aiohttp.ClientSession() as session:
-        statuses = [
-            MinecraftServerDiscordStatus(
-                message_id=966597522518867988,
-                server=JavaServer("raiblocksmc-play.com"),
-                webhook=Webhook.from_url(
-                    "https://discord.com/api/webhooks/966597376053739580/no",
-                    adapter=AsyncWebhookAdapter(session),
-                ),
-                title="RaiblocksMC Status",
-                thumbnail="https://raiblocksmc.com/storage/img/bigbigicon.png",
+        status = MinecraftServerDiscordStatus(
+            message_id=args.message_id,
+            server=JavaServer(args.server_host, args.server_port or 25565),
+            webhook=Webhook.from_url(
+                args.webhook,
+                adapter=AsyncWebhookAdapter(session),
             ),
-            MinecraftServerDiscordStatus(
-                message_id=966608694521528320,
-                server=JavaServer("example.com"),
-                webhook=Webhook.from_url(
-                    "https://discord.com/api/webhooks/966608280594026566/no",
-                    adapter=AsyncWebhookAdapter(session),
-                ),
-            ),
-        ]
+            title=args.title,
+            thumbnail=args.thumbnail,
+        )
 
         while True:
-            await asyncio.gather(*[status.update() for status in statuses])
+            await status.update()
             await asyncio.sleep(30)
 
 
